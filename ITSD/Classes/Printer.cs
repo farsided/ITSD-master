9b16c4dd508acf05d6205d100143ceb3be5e65a1
JM <mj.josel1013@gmail.com>
2023-06-20 11:43:50 +0800

    .

---
+++ ITSD/Classes/Printer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITSD.Classes
+{
+    public class Printer
+    {
+
+        public enum Status
+        {
+            Ongoing,
+            DOne
+        }
+
+        public enum Format
+        {
+            PDF,
+            Excel
+        }
+    }
+}
